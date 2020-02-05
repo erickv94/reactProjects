6a3e24f57afd5e70c7608509ff6bbcffeff18719
Erick Ventura <erick94111@gmail.com>
2020-02-04 22:35:49 -0600

    bbapp

--- breakingbad/src/App.js
+++ breakingbad/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Phrase = ({phrase}) =>{
+  return(
+    <div className="frase">
+      <h1>{phrase.quote}</h1>
+      <p>- {phrase.author}</p>
     </div>
-  );
+  )
+}
+
+const App = ()=>{
+  const [phrase, setPhrase]= useState({});
+
+  const fetchData = async ()=>{
+    const res = await axios("https://breaking-bad-quotes.herokuapp.com/v1/quotes")
+    setPhrase(res.data[0])
+  }
+
+  useEffect( 
+    () =>{
+      fetchData()
+    }
+  ,[])
+
+return (
+  <div className="contenedor">
+    <Phrase phrase={phrase}/>
+    <button
+      onClick={fetchData}
+    >New Phrase</button>
+  </div>
+)
 }
 
 export default App;
